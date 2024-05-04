43cc2ec3a83f38c00de545597be2608d91354777
Seogo <77395226+ksh6940@users.noreply.github.com>
2024-05-04 21:44:27 +0900

    Update Artibo_Test.py

--- Artibo_Test.py
+++ Artibo_Test.py
@@ -1,3 +1,4 @@
+#장애물 인식 코드 -> 장애물 인식하면 오른쪽으로 회전
 while True:
     artibo_move("Go", 50, 1)
     
@@ -12,3 +13,15 @@ while True:
         artibo_display_text("장애물 없음", 27)      
         artibo_move("Go", 100, 1)
         break
+
+#라인트레이서 코드 (테스트) -> 오른쪽 센서 기준값 (어두운 색일수록 값이 작음)
+
+RightValue = get_left_floor_sensor_value()
+LeftValue = get_left_floor_sensor_value()
+
+if RightValue < LeftValue:
+    artibo_move("Go", 10, 1)
+elif RightValue > LeftValue:
+    artibo_move("Right", 90, 1)
+else:
+    artibo_move("stop", 100, 1)
