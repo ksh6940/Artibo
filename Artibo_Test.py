feac8e4fa41de41bdfcad85e36c71d584bb98c05
Seogo <77395226+ksh6940@users.noreply.github.com>
2024-05-05 14:19:58 +0900

    Artibo_Test.py 업데이트

--- Artibo_Test.py
+++ Artibo_Test.py
@@ -1,10 +1,12 @@
-#장애물 인식 코드 -> 장애물 인식하면 오른쪽으로 회전
+#장애물 인식 코드 -> 장애물 (20cm 거리 이내) 인식하면 우회전
 while True:
     artibo_move("Go", 50, 1)
     
+    #장애물 인식 후 True/False로 로봇에 표시
     obstacle = is_obstacle_detected()
     artibo_display_text(obstacle, 27)
 
+    #장애물이 있으면 우회전 / 없으면 직진
     if obstacle == True:
         artibo_display_text("장애물 존재함", 27)
         artibo_move("Right", 100, 1)
@@ -14,14 +16,8 @@ while True:
         artibo_move("Go", 100, 1)
         break
 
-#라인트레이서 코드 (테스트) -> 오른쪽 센서 기준값 (어두운 색일수록 값이 작음)
+#라인트레이서 코드 (테스트) → 어두울수록 값의 크기가 작아짐
 
 RightValue = get_left_floor_sensor_value()
 LeftValue = get_left_floor_sensor_value()
 
-if RightValue < LeftValue:
-    artibo_move("Go", 10, 1)
-elif RightValue > LeftValue:
-    artibo_move("Right", 90, 1)
-else:
-    artibo_move("stop", 100, 1)
